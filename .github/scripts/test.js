2a142b361dc53add902cf6fb16ca6e862ec36134
vasildimitrovyara <vassil.dimitrov@yara.com>
2023-02-15 22:19:59 +0200

    test_6

---
+++ .github/scripts/test.js
@@ -0,0 +1,31 @@
+const executeTeamAction = require('./utils/executeTeamAction');
+const findBranchThroughPR = require('../utils/findBranchThroughPR');
+
+module.exports = async ({ github, context, actionName }) => {
+    console.log(github, context, actionName);
+    //   try {
+//     const branchName = await findBranchThroughPR({
+//       github,
+//       repo: context.payload.repository.name,
+//       owner: context.payload.repository.owner.login,
+//       issueNumber: context.payload.issue.number,
+//     });
+
+//     const { comment } = context.payload;
+
+//     if (!comment || !comment.body) {
+//       console.log('context.payload', context.payload);
+//       throw new Error('Comment missing');
+//     }
+
+//     await executeTeamAction({
+//       actionName,
+//       github,
+//       context,
+//       branchName,
+//       comment,
+//     });
+//   } catch (e) {
+//     console.error(e);
+//   }
+};
