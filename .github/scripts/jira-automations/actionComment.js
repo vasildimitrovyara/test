1680a04a177c49004c4ca9f64440cc931fa54e81
vasildimitrovyara <vassil.dimitrov@yara.com>
2023-02-16 15:16:51 +0200

    feat(RT-1): Progress

--- .github/scripts/jira-automations/actionComment.js
+++ .github/scripts/jira-automations/actionComment.js
@@ -2,31 +2,32 @@ const executeTeamAction = require('./utils/executeTeamAction');
 const findBranchThroughPR = require('../utils/findBranchThroughPR');
 
 module.exports = async ({ github, context, actionName }) => {
-  try {
-    const branchName = await findBranchThroughPR({
-      github,
-      repo: context.payload.repository.name,
-      owner: context.payload.repository.owner.login,
-      issueNumber: context.payload.issue.number,
-    });
+  console.log(actionName)
+  // try {
+  //   const branchName = await findBranchThroughPR({
+  //     github,
+  //     repo: context.payload.repository.name,
+  //     owner: context.payload.repository.owner.login,
+  //     issueNumber: context.payload.issue.number,
+  //   });
 
-    const { comment } = context.payload;
+  //   const { comment } = context.payload;
 
-    console.log(comment);
+  //   console.log(comment);
 
-    if (!comment || !comment.body) {
-      console.log('context.payload', context.payload);
-      throw new Error('Comment missing');
-    }
+  //   if (!comment || !comment.body) {
+  //     console.log('context.payload', context.payload);
+  //     throw new Error('Comment missing');
+  //   }
 
-    await executeTeamAction({
-      actionName,
-      github,
-      context,
-      branchName,
-      comment,
-    });
-  } catch (e) {
-    console.error(e);
-  }
+  //   await executeTeamAction({
+  //     actionName,
+  //     github,
+  //     context,
+  //     branchName,
+  //     comment,
+  //   });
+  // } catch (e) {
+  //   console.error(e);
+  // }
 };
